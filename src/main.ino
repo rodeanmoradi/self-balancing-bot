21f38d5a254cf609089f64c183f287b5bf190b77
Rodean Moradi <rodean.moradi@gmail.com>
2026-04-20 21:07:19 -0400

    Updated
    Refactor PID controller and Kalman filter parameters for improved performance. Adjust motor control logic and update serial communication speed.

--- src/main.ino
+++ src/main.ino
@@ -1,44 +1,51 @@
 #include <ArduinoEigenDense.h>
 #include <Wire.h>
 #include <MPU6050.h>
+#include <math.h>
 
 using namespace Eigen;
 
 MPU6050 mpu;
 
 static unsigned long lastMicros;
-static const int ena = 10;
-static const int enb = 9;
-static const int in1 = 8;
-static const int in2 = 7;
-static const int in3 = 13;
-static const int in4 = 12;
-static const float pi = 3.14159;
-static const float kP = 8.1;
-static const float kI = 0.0137;
-static const float kD = 0.1;
+static const int ena = 9;
+static const int enb = 10;
+static const int in1 = 7;
+static const int in2 = 8;
+static const int in3 = 12;
+static const int in4 = 13;
+static const float kP = 37.0;
+static const float kI = 117.0;
+static const float kD = 2.2;
+
+// Hold robot at real balance point, read Kalman output from Serial
+// Monitor, set this to that value. 0.0 is a starting guess
+static const float DESIRED_ANGLE = 0.0;
+
 float prevError = 0.0;
 float integral = 0.0;
 float dt = 0.01;
+
+// State is [theta, b], where theta is the tilt angle and b is the gyro bias
 Eigen::Vector2f x;
-Eigen::Vector2f y;
-Eigen::Vector2f z;
+Eigen::Vector2f B;
+float y;
+float z;
 Eigen::Matrix<float, 2, 2> phi;
-Eigen::Matrix<float, 2, 2> K;
-Eigen::Matrix<float, 2, 2> H;
+Eigen::Matrix<float, 2, 1> K;
+Eigen::Matrix<float, 1, 2> H;
 Eigen::Matrix<float, 2, 2> P;
 Eigen::Matrix<float, 2, 2> I;
 Eigen::Matrix<float, 2, 2> Q;
-Eigen::Matrix<float, 2, 2> R;
-Eigen::Matrix<float, 2, 2> S;
+float R;
+float S;
 
 float kalman(float accel, float gyro);
 float pidController(float tiltAngle, float kP, float kI, float kD);
 
-
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
   mpu.initialize();
   mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
@@ -51,29 +58,25 @@ void setup() {
   pinMode(in4, OUTPUT);
 
   lastMicros = micros();
-  // Angle
   x(0) = 0.0;
-  // Angular velocity
   x(1) = 0.0;
-  y(0) = 0.0;
-  y(1) = 0.0;
-  z(0) = 0.0;
-  z(1) = 0.0;
-  phi << 1, dt,
-         0, 1;
+  B(0) = dt;
+  B(1) = 0.0;
+  y = 0.0;
+  z = 0.0;
+  phi << 1.0, -dt,
+         0,   1.0;
   K.setZero();
-  H << 1, 0,
-       0, 1;
-  P << 4, 0,
-       0, 100;
+  H(0) = 1.0;
+  H(1) = 0.0;
+  P << 4.0, 0.0,
+       0.0, 100.0;
   I.setIdentity();
   Q.setZero();
-  Q << 0.001, 0,
-       0,     0.03;
-  R.setZero();
-  R << 0.03, 0,
-       0,    0.01;
-  S.setZero();
+  Q << 0.001, 0.0,
+       0.0,   0.03;
+  R = 0.5;
+  S = 0.0;
 
 }
 
@@ -82,100 +85,100 @@ void loop() {
   unsigned long now = micros();
   dt = (now - lastMicros) * 1e-6;
   lastMicros = now;
-  phi(0,1) = dt;
-  
+  phi(0,1) = -dt;
+  B(0) = dt;
+
   float aYRaw = mpu.getAccelerationY();
   float aZRaw = mpu.getAccelerationZ();
   float ay = aYRaw / 16384.0;
   float az = aZRaw / 16384.0;
-  float accel = atan2(ay, az) * 180.0 / pi;
+  float accel = atan2(ay, az) * 180.0 / M_PI;
   float omegaRaw = mpu.getRotationX();
   float gyro = omegaRaw / 131.0;
 
   float tiltAngle = kalman(accel, gyro);
-  float u = pidController(tiltAngle, kP, kI, kD);
-  float uPWM = constrain(u, -255.0, 255.0);
+  float u_controller = pidController(tiltAngle, kP, kI, kD);
+  //if(abs(u_controller) < 70.0) {
+    //if(u_controller < 0.0) {
+      //u_controller = -70.0;
+    //}
+    //else if(u_controller > 0.0) {
+      //u_controller = 70.0;
+    //}
+  //}
+  float uPWM = constrain(u_controller, -255.0, 255.0);
 
-  // Visualize Kalman
-  Serial.print(accel);
-  Serial.print(",");
-  Serial.println(tiltAngle);
+  //Serial.print(accel);
+  //Serial.print(",");
+  //Serial.println(tiltAngle);
 
-  // Visualize PID
-  // Serial.println(uPWM);
-
-
-  if (tiltAngle > 0) {
-
-    // Move forward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    analogWrite(ena, abs(uPWM));
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    analogWrite(enb, abs(uPWM));
-
-  } 
-  
-  else if (tiltAngle < 0) {
-
-    // Move backward
+  // 45 may be too high
+  if (abs(tiltAngle) > 30) {
     digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    analogWrite(ena, abs(uPWM));
+    digitalWrite(in2, LOW);
+    analogWrite(ena, 0);
     digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    analogWrite(enb, abs(uPWM));
-
+    digitalWrite(in4, LOW);
+    analogWrite(enb, 0);
+    integral = 0.0;
+    prevError = 0.0;
+  }
+  else {
+    if (u_controller > 0) {
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      analogWrite(ena, int(abs(uPWM)));
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      analogWrite(enb, int(abs(uPWM)));
+    }
+    else if (u_controller < 0) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      analogWrite(ena, int(abs(uPWM)));
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(enb, int(abs(uPWM)));
+    }
   }
 
 }
 
-// TODO: Add gyro bias
 float kalman(float accel, float gyro) {
 
-  z(0) = accel;
-  z(1) = gyro;
+  z = accel;
 
-  y = z - H * x;
-
+  // Predict FIRST, then compute innovation.
   // Predict
-  x = phi * x;
+  x = phi * x + B * gyro;
   P = phi * P * phi.transpose() + Q;
 
-  // Compute Kalman gain
-  S = H * P * H.transpose() + R;
-  K = (P * H.transpose()) * S.inverse();
+  // Innovation (after predict)
+  y = z - (H * x)(0, 0);
+
+  // Kalman gain
+  S = (H * P * H.transpose())(0, 0) + R;
+  K = (P * H.transpose()) / S;
 
   // Correct
   x = x + K * y;
-
-  // Update predicted error covariance
   P = (I - K * H) * P;
 
-  float tiltAngle = x(0);
-  return tiltAngle;
+  return x(0);
 
 }
 
 float pidController(float tiltAngle, float kP, float kI, float kD) {
 
-  float desiredAngle = 0.0;
+  float e = DESIRED_ANGLE - tiltAngle;
 
-  // Calculate error
-  float e = desiredAngle - tiltAngle;
-
-  // Calculate integral term
   integral += e * dt;
-
-  // Calculate derivative term
+  integral = constrain(integral, -75.0, 75.0);
   float derivative = (e - prevError) / dt;
-
-  // Calculate control input
-  float u = kP * e + kI * integral + kD * derivative;
+  float u_controller = kP * e + kI * integral + kD * derivative;
 
   prevError = e;
-
-  return u;
+  
+  return u_controller;
 
 }
